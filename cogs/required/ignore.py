ca1fa310fd5e9663081fde8b0849fa8632c284ce
Matthew <webguywp@gmail.com>
2019-12-03 13:08:40 -0500

    ignore user per guild

--- cogs/required/ignore.py
+++ cogs/required/ignore.py
@@ -7,18 +7,56 @@ class Ignore(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # ignore a user command
+    # ignore user command
     @commands.command()
     @commands.is_owner()
     async def ignore(self, ctx, member : discord.Member):
         try:
-            with open(r'config.json', 'r') as file:
-                json_data = json.load(file)
-                json_data['ignored']['users'].append(str(member))
-            with open(r'config.json', 'w') as file:
-                json.dump(json_data, file, indent=2)
+            with open(r'guildconfig.json', 'r') as file: # loads json file
+                json_data = json.load(file) # gets json data
+            try:
+                x = json_data[str(ctx.guild.id)]
+                print("1")
+                try:
+                    x = json_data[str(ctx.guild.id)]['ignored']
+                except:
+                    json_data[str(ctx.guild.id)]['ignored'] = []
+            except:
+                json_data[str(ctx.guild.id)] = {}
+                json_data[str(ctx.guild.id)]['ignored'] = []
+                with open(r'guildconfig.json', 'w') as file: # opens json file
+                    json.dump(json_data, file, indent=2) # writes to json file
+
+            if str(member) not in json_data[str(ctx.guild.id)]['ignored']:
+                json_data[str(ctx.guild.id)]['ignored'].append(str(member)) # appends user id to list
+            with open(r'guildconfig.json', 'w') as file: # opens json file
+                json.dump(json_data, file, indent=2) # writes to json file
         except:
-            await ctx.send("error")
+            await ctx.send("error") # error message
+
+    # unignore user command
+    @commands.command()
+    @commands.is_owner()
+    async def unignore(self, ctx, member : discord.Member):
+        try:
+            with open(r'guildconfig.json', 'r') as file: # loads json file
+                json_data = json.load(file) # gets json data
+            userList = json_data[str(ctx.guild.id)]['ignored'] # gets ignored users list
+            
+            try:
+                # removes user from list
+                for item in range(0, len(userList)):
+                    if str(member) == userList[item]:
+                        userList.pop(item)
+                        break
+            except:
+                await ctx.send("User not found") # if user isn't in list
+
+            json_data[str(ctx.guild.id)]['ignored'] = userList # saves new list to json array
+            with open(r'guildconfig.json', 'w') as file: # opens json file
+                json.dump(json_data, file, indent=2) # writes to json file
+        except:
+            await ctx.send("error") # error message
         
 def setup(bot):
     bot.add_cog(Ignore(bot))
--- cogs/utils/commandchecks.py
+++ cogs/utils/commandchecks.py
@@ -5,42 +5,50 @@ import json
 
 # command check function
 def allowedUser(ctx):
-    ### checks if user is in ignored list
-    try:
-        with open(r'config.json', 'r') as file:
-            user_data = json.load(file)
-            ignoredUsers = user_data['ignored']['users']
-        if str(ctx.author) in ignoredUsers:
-            return False
-    except:
-        logger.outputWrite(f'Command Check Failure')
-
     ### checks if command is enabled in guild
     try:
-        with open('commandconfig.json', 'r') as json_data_file:
-            commandconfig = json.load(json_data_file)
+        with open('guildconfig.json', 'r') as json_data_file:
+            guildconfig = json.load(json_data_file)
         guildID = str(ctx.guild.id)
         name = str(ctx.command.qualified_name)
         cog = str(ctx.command.cog.qualified_name)
         try:
-            value = commandconfig[guildID]
+            value = guildconfig[guildID]
             try:
-                value = commandconfig[guildID][cog]
+                value = guildconfig[guildID]["Commands"]
                 try:
-                    value = commandconfig[guildID][cog][name]
-                    return value
+                    value = guildconfig[guildID]["Commands"][cog]
+                    try:
+                        value = guildconfig[guildID]["Commands"][cog][name]
+                        return value
+                    except:
+                        guildconfig[guildID]["Commands"][cog][name] = False
                 except:
-                    commandconfig[guildID][cog][name] = False
+                    guildconfig[guildID]["Commands"][cog] = {}
+                    guildconfig[guildID]["Commands"][cog][name] = False
             except:
-                commandconfig[guildID][cog] = {}
-                commandconfig[guildID][cog][name] = False
+                guildconfig[guildID]["Commands"] = {}
         except:
-            commandconfig[guildID] = {}
-            commandconfig[guildID][cog] = {}
-            commandconfig[guildID][cog][name] = False
-        with open(r'commandconfig.json', 'w') as json_data_file:
-            json.dump(commandconfig, json_data_file, indent=2)
+            guildconfig[guildID] = {}
+            guildconfig[guildID]["Commands"] = {}
+            guildconfig[guildID]["Commands"][cog] = {}
+            guildconfig[guildID]["Commands"][cog][name] = False
+        with open(r'guildconfig.json', 'w') as json_data_file:
+            json.dump(guildconfig, json_data_file, indent=2)
         return False
     except:
-        logger.outputWrite(f'Command Check Failure')
+        logger.outputWrite(f'Command Check Failure (guild)')
         return False
+
+    ### checks if user is in ignored list
+    try:
+        with open(r'config.json', 'r') as file:
+            user_data = json.load(file)
+        try:
+            ignoredUsers = user_data[str(ctx.guild.id)]['ignored']
+            if str(ctx.author) in ignoredUsers:
+                return False
+        except:
+            pass
+    except:
+        logger.outputWrite(f'Command Check Failure (ignored user)')
