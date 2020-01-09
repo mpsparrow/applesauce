45d86e214077cedb05d49f549495e5a27ca375a0
Matthew <webguywp@gmail.com>
2020-01-09 14:14:20 -0500

    responses when pinged

--- cogs/main/responses.py
+++ cogs/main/responses.py
@@ -1,11 +1,12 @@
 '''
 Name: Responses
 Description: random responses to things
-Last Updated: January 8, 2020
+Last Updated: January 9, 2020
 Created: January 8, 2020
 '''
 import discord
 from discord.ext import commands
+import random
 
 class Responses(commands.Cog):
     def __init__(self, bot):
@@ -13,8 +14,15 @@ class Responses(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        # Deep Blue and bot pinged
         if ("<@!568271450176356352>" in message.content) and ("<@!657709911337074698>" in message.content):
-            await message.channel.send(f"Stop pinging us {message.author.mention} <:pinged:451198700832817202>")
+            await message.channel.send(f"{message.author.mention} <:pinged:451198700832817202>")
+            return
+
+        # bot pinged
+        if ("<@!568271450176356352>" in message.content) and (random.choice([1,2,3,4] == 1)):
+            await message.channel.send(f"{message.author.mention} <:pinged:451198700832817202>")
+            return
 
 def setup(bot):
     bot.add_cog(Responses(bot))
