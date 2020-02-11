97f636e71c811e8c0d960386f4d65c6f0fa005dd
Matthew <matthew@younginwp.com>
2020-02-11 17:55:45 -0500

    switched to different wiki library

--- cogs/main/wiki.py
+++ cogs/main/wiki.py
@@ -1,13 +1,13 @@
 '''
 Name: Wikipedia
 Description: Wikipedia command
-Last Updated: January 19, 2020
+Last Updated: February 11, 2020
 Created: October 30, 2019
 '''
 import discord
 from discord.ext import commands
 from utils import commandchecks
-import wikipedia
+import wikipediaapi
 
 class Wiki(commands.Cog):
     def __init__(self, bot):
@@ -16,15 +16,16 @@ class Wiki(commands.Cog):
     # wikipedia (command)
     @commands.check(commandchecks.isAllowed)
     @commands.command(name="wikipedia", description="Queries Wikipedia and returns summary and link to the page.", usage="wikipedia <query>", aliases=['wiki'])
-    @commands.cooldown(1, 10, commands.BucketType.default)
-    async def wikipedia(self, ctx, *, lookup):
+    @commands.cooldown(1, 10, commands.BucketType.guild)
+    async def wikipedia(self, ctx, *, lookup: str):
         loading = await ctx.send('Searching for article....') # loading message
 
         try:
-            page = wikipedia.summary(lookup, sentences=1) # gets pages first sentence
-            wikiPage = wikipedia.page(lookup)
-            pageURL = wikiPage.url # gets pages URL
-            pageTitle = wikiPage.title
+            wiki = wikipediaapi.Wikipedia('en')
+            wikipage = wiki.page(lookup)
+            page = wiki.extracts(wikipage, exsentences=2)
+            pageURL = wikipage.fullurl
+            pageTitle = wikipage.title
         except:
             # if nothing is found
             page = 'No results found.'
@@ -33,11 +34,9 @@ class Wiki(commands.Cog):
 
         # embed for results
         embed=discord.Embed(title='Wikipedia', description=f'{pageURL}', color=0xc1c100)
-        embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png")
-        # embed.add_field(name='Lookup', value=f'{lookup}', inline=False)
+        embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Wikipedia_svg_logo.svg/200px-Wikipedia_svg_logo.svg.png")
         embed.add_field(name=f'{pageTitle}', value=f'{page}', inline=False)
         await ctx.send(embed=embed)
-
         await loading.delete() # deletes loading message
 
 def setup(bot):
