fe9b8bd11dc8ebf7a994fda343e81879008a29b7
Matthew <webguywp@gmail.com>
2020-02-11 23:28:54 -0500

    removed debug command

--- cogs/main/reaction.py
+++ cogs/main/reaction.py
@@ -12,17 +12,5 @@ class Reaction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # chance (command)
-    @commands.command()
-    @commands.has_permissions(manage_guild=True)
-    async def test(self, ctx):
-        '''
-        dbConnect.prefix(ctx.guild.id, "!")
-        dbConnect.commands(ctx.guild.id, "ball8", True, 1)
-        dbConnect.ignore(ctx.guild.id, f"{ctx.message.author.id}", False)
-        '''
-        dbConnect.commandCount(ctx.guild.id, "ping")
-        await ctx.send("worked?")
-
 def setup(bot):
     bot.add_cog(Reaction(bot))
