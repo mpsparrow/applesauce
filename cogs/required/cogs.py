aca5fdca2faf77f176f70d82ad0551df6c47a725
Matthew <matthew@younginwp.com>
2020-05-16 05:39:57 -0400

    cog commands rewrite

--- cogs/required/cogs.py
+++ cogs/required/cogs.py
@@ -1,124 +1,120 @@
-# Cogs cog
-# For managing the loading and enabling of cogs
+'''
+Owner level cog management.
+'''
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
-from util import logger, dbInsert, dbQuery
+from util import runLog, dbInsert
 
-
-class cogCmds(commands.Cog):
+        
+class cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # commands
-    # restart extension
-    @commands.command()
+    @commands.group(name="cog", description="Controlling cogs at owner level.", usage="cog <action> <cog name>", aliases=["c"])
     @commands.is_owner()
-    async def reloadCog(self, ctx, extension):
+    async def cog(self, ctx):
+        pass
+
+
+    @cog.command(name="enable", description="Enable loading of cog on bot startup.", usage="cog enable <cog name>", aliases=["e"])
+    async def enable(self, ctx, extension):
         try:
-            state = dbQuery.cogEnabled(extension)
-            self.bot.unload_extension(f'cogs.main.{extension}')
-            dbInsert.cogs(extension, state, False)
-            self.bot.load_extension(f'cogs.main.{extension}')
-            dbInsert.cogs(extension, state, True)
-            logger.normRun(f'Successfully reloaded cog: {extension}')
-            await ctx.message.add_reaction("✅")
-        except Exception as e:
-            logger.errorRun(f'Failed to reload cog: {extension}')
-            logger.errorRun(f'{e}')
+            db # update status
+        except CogStateError:
+            # The cog state was unable to be changed.
+            runLog.error(f"Cog '{extension}' failed to change startup state. CogStateError (cog.enable)")
             await ctx.message.add_reaction("❌")
-
-    # unload extension
-    @commands.command()
-    @commands.is_owner()
-    async def unloadCog(self, ctx, extension):
-        try:
-            self.bot.unload_extension(f'cogs.main.{extension}')
-            logger.normRun(f'Successfully unloaded cog: {extension}')
-            state = dbQuery.cogEnabled(extension)
-            dbInsert.cogs(extension, state, False)
-            await ctx.message.add_reaction("✅")
-        except Exception as e:
-            logger.errorRun(f'Failed to unload cog: {extension}')
-            logger.errorRun(f'{e}')
-            await ctx.message.add_reaction("❌")
-
-    # unload extension
-    @commands.command()
-    @commands.is_owner()
-    async def loadCog(self, ctx, extension):
-        try:
-            self.bot.load_extension(f'cogs.main.{extension}')
-            logger.normRun(f'Successfully loaded cog: {extension}')
-            state = dbQuery.cogEnabled(extension)
-            dbInsert.cogs(extension, state, True)
-            await ctx.message.add_reaction("✅")
-        except Exception as e:
-            logger.errorRun(f'Failed to load cog: {extension}')
-            logger.errorRun(f'{e}')
-            await ctx.message.add_reaction("❌")
-
-    # enable extension
-    @commands.command()
-    @commands.is_owner()
-    async def enableCog(self, ctx, extension):
-        flag = False
-        try:
-            self.bot.load_extension(f'cogs.main.{extension}')
-            self.bot.unload_extension(f'cogs.main.{extension}')
-            flag = True
-        except:
-            try:
-                self.bot.unload_extension(f'cogs.main.{extension}')
-                self.bot.load_extension(f'cogs.main.{extension}')
-                flag = True
-            except:
-                pass
-
-        if flag:
-            try:
-                state = dbQuery.cogLoaded(extension)
-                dbInsert.cogs(extension, True, state)
-                logger.normRun(f'Successfully enabled cog: {extension}')
-                await ctx.message.add_reaction("✅")
-            except Exception as e:
-                logger.errorRun(f'Failed to enable cog: {extension}')
-                logger.errorRun(f'{e}')
-                await ctx.message.add_reaction("❌")
         else:
-            logger.errorRun(f'Failed to enable cog: {extension}')
-            await ctx.message.add_reaction("❌")
+            runLog.log(f"Cog '{extension}' enabled on startup. (cog.enable)")
+            await ctx.message.add_reaction("✅")
 
-    # disable extension
-    @commands.command()
-    @commands.is_owner()
-    async def disableCog(self, ctx, extension):
-        flag = False
+
+    @cog.command(name="disable", description="Disable loading of cog on bot startup.", usage="cog disable <cog name>", aliases=["d"])
+    async def disable(self, ctx, extension):
         try:
-            self.bot.load_extension(f'cogs.main.{extension}')
-            self.bot.unload_extension(f'cogs.main.{extension}')
-            flag = True
-        except:
-            try:
-                self.bot.unload_extension(f'cogs.main.{extension}')
-                self.bot.load_extension(f'cogs.main.{extension}')
-                flag = True
-            except:
-                pass
-            
-        if flag:
-            try:
-                state = dbQuery.cogLoaded(extension)
-                dbInsert.cogs(extension, False, state)
-                logger.normRun(f'Successfully disabled cog: {extension}')
-                await ctx.message.add_reaction("✅")
-            except Exception as e:
-                logger.errorRun(f'Failed to disable cog: {extension}')
-                logger.errorRun(f'{e}')
-                await ctx.message.add_reaction("❌")
-        else:
-            logger.errorRun(f'Failed to disable cog: {extension}')
+            db # update status
+        except CogStateError:
+            # The cog state was unable to be changed.
+            runLog.error(f"Cog '{extension}' failed to change startup state. CogStateError (cog.disable)")
             await ctx.message.add_reaction("❌")
+        else:
+            runLog.log(f"Cog '{extension}' disabled on startup. (cog.disable)")
+            await ctx.message.add_reaction("✅")
+
+
+    @cog.command(name="load", description="Load cog while bot is running.", usage="cog load <cog name>", aliases=["l"])
+    async def load(self, ctx, extension):
+        try:
+            # Attempt to load cog.
+            self.bot.load_extension(f'cogs.main.{extension}')
+        except ExtensionNotFound:
+            # The cog could not be found.
+            runLog.error(f"Cog '{extension}' was not found. ExtensionNotFound (cog.load)")
+            await ctx.message.add_reaction("❌")
+        except ExtensionAlreadyLoaded:
+            # The cog was already loaded.
+            runLog.warn(f"Cog '{extension}' was already loaded. ExtensionAlreadyLoaded (cog.load)")
+            await ctx.message.add_reaction("✅")
+        except NoEntryPointError:
+            # The cog does not have a setup function.
+            runLog.error(f"Cog '{extension}' has no setup function. NoEntryPointError (cog.load)")
+            dbInsert.cogs(extension, False) # Set cog state to not loaded in database.
+            await ctx.message.add_reaction("⚠️")
+        except ExtensionFailed:
+            # The cog setup function has an execution error.
+            runLog.error(f"Cog '{extension}' has an execution error. ExtensionFailed (cog.load)")
+            dbInsert.cogs(extension, False) # Set cog state to not loaded in database.
+            await ctx.message.add_reaction("⚠️")
+        else:
+            runLog.log(f"Cog '{extension}' loaded. (cog.load)")
+            dbInsert.cogs(extension, True) # Set cog state to loaded in database.
+            await ctx.message.add_reaction("✅")
+
+
+    @cog.command(name="unload", description="Unload cog while bot is running.", usage="cog unload <cog name>", aliases=["u"])
+    async def unload(self, ctx, extension):
+        try:
+            # Attempt to unload cog.
+            self.bot.unload_extension(f'cogs.main.{extension}')
+        except ExtensionNotLoaded:
+            # The cog was not found or unloaded.
+            runLog.error(f"Cog '{extension}' was unable to be unloaded and/or found. ExtensionNotLoaded (cog.unload)")
+            await ctx.message.add_reaction("❌")
+        else:
+            runLog.log(f"Cog '{extension}' unloaded. (cog.unload)")
+            dbInsert.cogs(extension, False) # Set cog state to not loaded in database.
+            await ctx.message.add_reaction("✅")   
+
+
+    @cog.command(name="reload", description="Reload cog while bot is running.", usage="cog reload <cog name>", aliases=["r"])
+    async def reload(self, ctx, extension):
+        try:
+            # Attempt to reload cog.
+            self.bot.reload_extension(f'cogs.main.{extension}')
+        except ExtensionNotLoaded:
+            # The cog was not reloaded.
+            runLog.error(f"Cog '{extension}' was unable to be reloaded. ExtensionNotLoaded (cog.reload)")
+            dbInsert.cogs(extension, False) # Set cog state to not loaded in database.
+            await ctx.message.add_reaction("⚠️")
+        except ExtensionNotFound:
+            # The cog could not be found.
+            runLog.warn(f"Cog '{extension}' was not found. ExtensionNotFound (cog.reload)")
+            await ctx.message.add_reaction("❌")
+        except NoEntryPointError:
+            # The cog does not have a setup function.
+            runLog.error(f"Cog '{extension}' has no setup function. NoEntryPointError (cog.reload)")
+            dbInsert.cogs(extension, False) # Set cog state to not loaded in database.
+            await ctx.message.add_reaction("⚠️")
+        except ExtensionFailed:
+            # The cog setup function has an execution error.
+            runLog.error(f"Cog '{extension}' has an execution error. ExtensionFailed (cog.reload)")
+            dbInsert.cogs(extension, False) # Set cog state to not loaded in database.
+            await ctx.message.add_reaction("⚠️")
+        else:
+            runLog.info(f"Cog '{extension}' reloaded. (cog.reload)")
+            dbInsert.cogs(extension, True) # Set cog state to loaded in database.
+            await ctx.message.add_reaction("✅")           
 
 def setup(bot):
-    bot.add_cog(cogCmds(bot))
+    bot.add_cog(cog(bot))
