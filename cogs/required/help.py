058747fab0e543858d85383ada654156f5b2b965
Matthew <matthew@younginwp.com>
2020-05-31 01:22:47 -0400

    help command

--- cogs/required/help.py
+++ cogs/required/help.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 from util import embed as emb
+from util.db.query import queryPrefix
 
 class Help(commands.Cog):
     """
@@ -19,10 +20,10 @@ class SetupHelp(commands.MinimalHelpCommand):
     Main help command.
     """
     async def command_not_found(self, string):
-        await self.context.send(emb.make_error("Command not found."))
+        await self.context.send(embed=emb.make_error("Command not found."))
 
     async def subcommand_not_found(self, command, string):
-        await self.context.send(emb.make_error("Sub-command not found."))
+        await self.context.send(embed=emb.make_error("Sub-command not found."))
 
     async def send_cog_help(self, cog):
         await self.context.send("Cog help")
@@ -34,7 +35,9 @@ class SetupHelp(commands.MinimalHelpCommand):
         await self.context.send("command help")
 
     async def send_bot_help(self, mapping):
-        await self.context.send("bot help")
+        embed = emb.make("Help", f"Specify a command/cog to get further information `{queryPrefix.prefix(self.context.guild.id)}help <command>`")
+        embed.add_field(name="Commands", value=f"some commands", inline=False)
+        await self.context.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Help(bot))
