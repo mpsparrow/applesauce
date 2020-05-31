64e549930969bcc3d544ebffe5bf756d8f563008
Matthew <matthew@younginwp.com>
2020-05-30 22:52:21 -0400

    testing

--- util/checks/command.py
+++ util/checks/command.py
@@ -15,7 +15,10 @@ def isAllowed(ctx):
         guildID = ctx.guild.id
         cogName = ctx.command.cog
         author = ctx.message.author.id
-        value = queryCogGuild.status(guildID, cogName) and not(queryIgnore.status(guildID, author))
+        value = queryCogGuild.status(guildID, cogName)
+        print(value)
+        value2 = not(queryIgnore.status(guildID, author)
+        print(value2)
     except Exception:
         runLog.error("isAllowed error (checks.command)")
         return False
