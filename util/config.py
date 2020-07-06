55bd29700dd92298b3dc6994c050a0c4694b3c70
Matthew <matthew@younginwp.com>
2020-07-06 02:19:10 -0400

    fixed exceptions

--- util/config.py
+++ util/config.py
@@ -10,13 +10,13 @@ def readINI(filename: str):
     Read ini file
     :param str filename: ini file name
     :return: parsable ini
-    :raises configReadError: if it fails to read or access ini file
+    :raises configError: if it fails to read or access ini file
     """
     try:
         conf = configparser.ConfigParser()
         conf.read(filename)
     except:
-        raise exceptions.configReadError
+        raise exceptions.configError("Failed to read (config.readINI)")
     else:
         return conf
 
@@ -24,26 +24,26 @@ def dumpINI(write):
     """
     Write to ini file
     :param write: contents to write to ini file
-    :raises configWriteError: if it fails to write to ini file
+    :raises configError: if it fails to write to ini file
     """
     try:
         conf = configparser.ConfigParser()
         conf.write(write)
     except:
-        raise exceptions.configWriteError
+        raise exceptions.configError("Failed to write (config.dumpINI)")
 
 def readJSON(filename: str):
     """
     Read json file
     :param str filename: json file name
     :return: parsable json
-    :raises configReadError: if it fails to read or access json file
+    :raises configError: if it fails to read or access json file
     """
     try:
         with open(filename, 'r') as jsondata:
             data = json.load(jsondata)
     except:
-        raise exceptions.configReadError
+        raise exceptions.configError("Failed to read (config.readJSON)")
     else:
         return data
 
@@ -52,10 +52,10 @@ def dumpJSON(filename: str, data):
     Write to json file
     :param str filename: json file name
     :param write: contents to write to json file
-    :raises configWriteError: if it fails to write to json file
+    :raises configError: if it fails to write to json file
     """
     try:
         with open(filename, 'w') as jsondata:
             json.dump(data, jsondata, indent=4)
     except:
-        raise exceptions.configWriteError
+        raise exceptions.configError("Failed to write (config.dumpJSON)")
