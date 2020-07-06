f751cdbba3b24db409b0f067fda33b915d056d3a
Matthew <matthew@younginwp.com>
2020-07-06 02:19:25 -0400

    comment tweaks

--- cogs/main/moderation.py
+++ cogs/main/moderation.py
@@ -10,21 +10,18 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # ban (command)
     @commands.check(command.isAllowed)
     @commands.command(name="ban", description="Bans user from the guild.", usage="ban <user> <reason>")
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         await member.ban(reason=reason)
 
-    # clear (command)
     @commands.check(command.isAllowed)
     @commands.command(name="clear", description="Clears x amount of messages from channel (including the message containing the command call).", usage="clear <#messages>", aliases=['purge'])
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=1):
         await ctx.channel.purge(limit=amount+1)
 
-    # kick (command)
     @commands.check(command.isAllowed)
     @commands.command(name="kick", description="Kicks user from the guild.", usage="kick <user>")
     @commands.has_permissions(kick_members=True)
