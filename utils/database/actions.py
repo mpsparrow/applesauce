f4f2fc107806c4b457c0e2f6929b0dc26298e39d
Matthew <matthew@younginwp.com>
2020-08-01 02:55:31 -0400

    database connection

--- utils/database/actions.py
+++ utils/database/actions.py
@@ -1,2 +1,23 @@
-import pymongo
+from pymongo import MongoClient
 from utils.config import readINI
+
+def connect():
+    """
+    Connects to main database
+    :return: client
+    """
+    conf = readINI("config.ini")["MongoDB"]
+    client = MongoClient(f"mongodb://{conf['user']}:{conf['password']}@{conf['host']}?authSource={conf['database']}")
+    return client
+
+def connectC(user: str, password: str, host: str, database: str):
+    """
+    Connects to custom database
+    :param str name: username
+    :param str password: password
+    :param str host: host URL/IP
+    :param str database: database name
+    :return: client
+    """
+    client = MongoClient(f"mongodb://{user}:{password}@{host}?authSource={database}")
+    return client
