73192d5aaf1326192e1098d3b4a6d9a28428fff2
Matthew <matthew@younginwp.com>
2020-08-06 21:33:01 -0400

    test db connection

--- utils/checks.py
+++ utils/checks.py
@@ -1,5 +1,7 @@
+from utils.database.actions import isConnected, connect
+
 def startupchecks():
     """
     Checks to make sure everything is in place before starting the bot
     """
-    return True
+    return isConnected(connect())
