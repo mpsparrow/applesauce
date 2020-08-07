e762c44c417f78c4ee13c97f898abeb7711068fc
Matthew <matthew@younginwp.com>
2020-08-06 20:46:30 -0400

    db connection testing

--- utils/database/actions.py
+++ utils/database/actions.py
@@ -1,9 +1,13 @@
+"""
+MongoDB connection and action functions
+"""
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from utils.config import readINI
 
 def connect():
     """
-    Connects to main database
+    Connects to main MongoDB database
     :return: client
     """
     conf = readINI("config.ini")["MongoDB"]
@@ -12,7 +16,7 @@ def connect():
 
 def connectC(user: str, password: str, host: str, database: str):
     """
-    Connects to custom database
+    Connects to custom MongoDB database
     :param str name: username
     :param str password: password
     :param str host: host URL/IP
@@ -21,3 +25,17 @@ def connectC(user: str, password: str, host: str, database: str):
     """
     client = MongoClient(f"mongodb://{user}:{password}@{host}?authSource={database}")
     return client
+
+def isConnected(client: MongoClient):
+    """
+    Checks MongoDB connection
+    :param client: client from MongoClient
+    :return: boolean value
+    """
+    try:
+        client.admin.command('ismaster')
+        return True
+    except ConnectionFailure:
+        return False
+    else:
+        return False
