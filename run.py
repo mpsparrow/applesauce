b38c963e1681cbd22e252fb2c5e806a71f5c5fce
Matthew <matthew@younginwp.com>
2020-08-06 20:45:58 -0400

    stop loading plugins in on_ready()

--- run.py
+++ run.py
@@ -18,8 +18,13 @@ import discord
 import importlib
 from discord.ext import commands
 from utils.config import readINI
-from utils.logger import log, startLog, clearLogs
 from utils.checks import startupchecks
+
+# create logs folder
+if not(os.path.isdir("logs")):
+    os.mkdir("logs")
+
+from utils.logger import log, startLog, clearLogs
 
 # command line arguments assigning
 parser = argparse.ArgumentParser(description="Applesauce - modular Discord bot framework based on discord.py")
@@ -40,61 +45,62 @@ logging.basicConfig(filename="logs/discord.log",level=logging.INFO) # system log
 # defines bot
 bot = commands.Bot(command_prefix=readINI("config.ini")["main"]["defaultPrefix"], case_insensitive=True)
 
-# on_ready() starts the bootup of the bot
+startLog.info("Starting Bot")
+
+# if safemode 
+if args.s:
+    startLog.info("Safemode Activated")
+    return
+
+# startup checks
+startLog.info("Running Checks")
+
+if startupchecks():
+    startLog.info("Startup Checks Passed")
+else:
+    startLog.error("Startup Checks Failed, System Aborting")
+    print("Startup Checks Failed")
+    print("logs\startup.log")
+    os._exit(1)
+
+# if safemode is not active
+if args.p:
+    # plugin loading
+    startLog.info("Starting Plugins")
+
+    for folder in ["core", "plugins"]:
+        for plugins in next(os.walk(folder))[1]:
+
+            # skips '__pycache__' folder
+            if plugins == "__pycache__":
+                continue
+
+            # tries to load extension
+            try:
+                bot.load_extension(f"plugins.{plugins}")
+                i = importlib.import_module(f"plugins.{plugins}")
+                startLog.info(f"Loaded: {i.PLUGIN_NAME} | Cogs: {i.COG_NAMES} | Version: {i.VERSION}")
+            except commands.ExtensionNotFound:
+                # The cog could not be found.
+                startLog.warning(f"plugins.{plugins}: not found (ExtensionNotFound)")
+            except commands.ExtensionAlreadyLoaded:
+                # The cog was already loaded.
+                startLog.warning(f"plugins.{plugins}: already loaded (ExtensionAlreadyLoaded)")
+            except commands.NoEntryPointError:
+                # The cog does not have a setup function.
+                startLog.error(f"plugins.{plugins}: no setup function (NoEntryPointError)")
+            except commands.ExtensionFailed:
+                # The cog setup function has an execution error.
+                startLog.error(f"plugins.{plugins}: execution error (ExtensionFailed)")
+            except Exception as error:
+                bot.unload_extension(f"plugins.{plugins}")
+                startLog.error(f"plugins.{plugins}: variables not properly defined. Plugin unloaded.")
+else:
+    startLog.info("Plugin Loading Skipped")
+
 @bot.event
 async def on_ready():
-    startLog.info("Starting Bot")
-
-    # if safemode 
-    if args.s:
-        startLog.info("Safemode Activated")
-        return
-
-    # startup checks
-    startLog.info("Running Checks")
-
-    if startupchecks():
-        startLog.info("Startup Checks Passed")
-    else:
-        startLog.error("Startup Checks Failed, System Aborting")
-        print("Startup Checks Failed")
-        print("logs\startup.log")
-        os._exit(1)
-
-    # if safemode is not active
-    if args.p:
-        # plugin loading
-        startLog.info("Starting Plugins")
-
-        for folder in ["core", "plugins"]:
-            for plugins in next(os.walk(folder))[1]:
-
-                # skips '__pycache__' folder
-                if plugins == "__pycache__":
-                    continue
-
-                # tries to load extension
-                try:
-                    bot.load_extension(f"plugins.{plugins}")
-                    i = importlib.import_module(f"plugins.{plugins}")
-                    startLog.info(f"Loaded: {i.PLUGIN_NAME} | Cogs: {i.COG_NAMES} | Version: {i.VERSION}")
-                except commands.ExtensionNotFound:
-                    # The cog could not be found.
-                    startLog.warning(f"plugins.{plugins}: not found (ExtensionNotFound)")
-                except commands.ExtensionAlreadyLoaded:
-                    # The cog was already loaded.
-                    startLog.warning(f"plugins.{plugins}: already loaded (ExtensionAlreadyLoaded)")
-                except commands.NoEntryPointError:
-                    # The cog does not have a setup function.
-                    startLog.error(f"plugins.{plugins}: no setup function (NoEntryPointError)")
-                except commands.ExtensionFailed:
-                    # The cog setup function has an execution error.
-                    startLog.error(f"plugins.{plugins}: execution error (ExtensionFailed)")
-                except Exception as error:
-                    bot.unload_extension(f"plugins.{plugins}")
-                    startLog.error(f"plugins.{plugins}: variables not properly defined. Plugin unloaded.")
-    else:
-        startLog.info("Plugin Loading Skipped")
+    startLog.info("Connected")
 
 # Starts bot with Discord token from config.ini
 bot.run(readINI("config.ini")["main"]["discordToken"])
