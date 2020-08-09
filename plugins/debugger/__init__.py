1c0254a1f265ded3e49ea148c4c5d78e8a442ce8
Matthew <matthew@younginwp.com>
2020-08-09 01:24:57 -0400

    debugging plugin

---
+++ plugins/debugger/__init__.py
@@ -0,0 +1,18 @@
+from .debugger import Debugger
+
+### PLUGIN INFO ###
+PLUGIN_NAME = "Debugger"
+COG_NAMES = ["Debugger"]
+VERSION = 0.1
+AUTHOR = "Matthew Sparrow"
+DESCRIPTION = "Debugging tools for owners"
+LOAD_ON_START = False
+REQUIRED = False
+
+def setup(bot):
+    """
+    Setup when registering plugin
+    """
+
+    # register cog
+    bot.add_cog(Debugger(bot))
---
+++ plugins/debugger/debugger.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Debugger(commands.Cog):
+    """
+    Debugging tools
+    """
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ping", description="Pings the bot and returns latency")
+    @commands.is_owner()
+    async def ping(self, ctx):
+        """
+        Pings the bot and returns latency
+        """
+        await ctx.send(f"pong! {bot.latency}")
