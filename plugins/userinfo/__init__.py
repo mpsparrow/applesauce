5f325ac0ff57b688cfcc913813c91e741b98ae55
Robert <robert.trololo@gmail.com>
2020-08-10 19:56:25 +0200

    Added plugininfo

--- plugins/userinfo/__init__.py
+++ plugins/userinfo/__init__.py
@@ -1,15 +1,5 @@
 from .userinfo import Userinfo
 
-### PLUGIN INFO ###
-PLUGIN_NAME = "Userinfo"
-COG_NAMES = ["Userinfo"]
-VERSION = 0.1
-AUTHOR = "Lauchmelder"
-DESCRIPTION = "A tool to provide discord user information"
-LOAD_ON_START = True
-REQUIRED = False
-HIDDEN = False
-
 def setup(bot):
     """
     Setup when registering plugin
---
+++ plugins/userinfo/plugininfo.py
@@ -0,0 +1,9 @@
+### PLUGIN INFO ###
+PLUGIN_NAME = "Userinfo"
+COG_NAMES = ["Userinfo"]
+VERSION = 0.1
+AUTHOR = "Lauchmelder"
+DESCRIPTION = "A tool to provide discord user information"
+LOAD_ON_START = True
+REQUIRED = False
+HIDDEN = False
