7b53afc1dbcb9c5e869b680065e24839d81b50cf
Matthew <matthew@younginwp.com>
2020-08-11 13:08:24 -0400

    check if user is ignored

--- utils/checks.py
+++ utils/checks.py
@@ -15,9 +15,11 @@ def is_guild_enabled():
         """
         try:
             pluginCol = connect()[readINI("config.ini")["MongoDB"]["database"]]["plugins"]
+            guildCol = connect()[readINI("config.ini")["MongoDB"]["database"]]["guilds"]
             pluginData = pluginCol.find_one({ "_id": str(ctx.command.cog).split('.')[1] })
+            guildData = guildCol.find_one({ "_id": str(ctx.guild.id) })
 
-            if pluginData["guilds"][str(ctx.guild.id)]:
+            if pluginData["guilds"][str(ctx.guild.id)] and guildData["ignore"][str(ctx.author.id)]:
                 return True
             return False
         except Exception:
