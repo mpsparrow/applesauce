71d82163bcf576c425dd4b49985c7afc3ac93940
Robert <robert.trololo@gmail.com>
2020-08-12 12:52:45 +0200

    Added chance command

---
+++ plugins/chance/__init__.py
@@ -0,0 +1,4 @@
+from .chance import Chance
+
+def setup(bot):
+    bot.add_cog(Chance(bot))
---
+++ plugins/chance/plugininfo.py
@@ -0,0 +1,9 @@
+### PLUGIN INFO ###
+PLUGIN_NAME = "Chance"
+COG_NAMES = ["Chance"]
+VERSION = 0.1
+AUTHOR = "Lauchmelder"
+DESCRIPTION = "Rates your chances of success"
+LOAD_ON_START = True
+REQUIRED = False
+HIDDEN = False
