0ec462e9e14048ca6ba514a1037e15c44c545bea
Robert <robert.trololo@gmail.com>
2020-08-13 00:55:00 +0200

    Basic trivia logic down

--- plugins/trivia/trivia.py
+++ plugins/trivia/trivia.py
@@ -1,17 +1,83 @@
 import discord
 from discord.ext import commands
+import time
+import random
 from utils.checks import is_guild_enabled
-from .triviaAPI import Quiz
+from .triviaAPI import topic_list, topics, difficulties, Quiz
 
-default_topic = Quiz.topic_list[0]
+default_topic = topic_list[0]
 default_amount = 10
-default_diff = Quiz.difficulties[0]
+default_diff = difficulties[0]
 
 max_questions = 20
+
+reaction_emotes = {
+    "A": "🇦",
+    "B": "🇧",
+    "C": "🇨",
+    "D": "🇩",
+    "True": "✅",
+    "False": "❌"
+}
+
+class TriviaSession:
+    def __init__(self, ctx, quiz, parent):
+        self.quiz = quiz
+        self.ctx = ctx
+        self.parent = parent
+        self.current_msg = None
+
+    async def start(self):
+        i = 0
+        for question in self.quiz.questions:
+            i += 1
+            await self.render_question(question, i)
+            time.sleep(5)
+            print("jksjdksd")
+
+        self.parent.activeObjects.pop(self.ctx.guild.id, None)
+
+    async def render_question(self, q, i):
+        title = ""
+        if q.type == "boolean":
+            title += "True or False?"
+        title += q.question
+
+        embed = discord.Embed(
+                title = f"Question {i}",
+                description = title,
+                color = 0xc1c100
+            )
+
+        if q.type == "multiple":
+            true_position = random.randint(0, 3)
+            j = 0
+            for k in range(0, 4):
+                if k == true_position:
+                    embed.add_field(name = f"{chr(65 + k)}. {q.answer}", value="\u200b", inline=False)
+                else:
+                    embed.add_field(name = f"{chr(65 + k)}. {q.wrong_answers[k]}", value="\u200b", inline=False)
+                    k += 1
+
+        embed.set_footer(text=f"{q.category} | {q.difficulty}")
+
+        self.current_msg = await self.ctx.send(embed=embed)
+        if q.type == "multiple":
+            await self.current_msg.add_reaction(reaction_emotes["A"])
+            await self.current_msg.add_reaction(reaction_emotes["B"])
+            await self.current_msg.add_reaction(reaction_emotes["C"])
+            await self.current_msg.add_reaction(reaction_emotes["D"])
+        else:
+            await self.current_msg.add_reaction(reaction_emotes["True"])
+            await self.current_msg.add_reaction(reaction_emotes["False"])
+
+
 
 class Trivia(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.activeObjects = {}
 
     @commands.group(name="trivia", description="Play a game of trivia!", usage="<subcommand>", aliases=["t"], invoked_subcommand=True)
     @is_guild_enabled()
@@ -28,7 +94,7 @@ class Trivia(commands.Cog):
         """
         embed = discord.Embed(
             title = "Trivia categories",
-            description = f"`{', '.join(Quiz.topic_list)}`",
+            description = f"`{', '.join(list(topics))}`",
             color = 0xc1c100
         )
         await ctx.send(embed=embed)
@@ -36,37 +102,51 @@ class Trivia(commands.Cog):
     @trivia.command(name="start", description="Start a game of trivia", usage="[topic] [questions] [difficulty]", aliases=["s"])
     @is_guild_enabled()
     async def start(self, ctx, 
-        topic: str = default_topic, 
+        topic: str = None, 
         amount: int = default_amount, 
-        difficulty: str = default_diff):
+        difficulty: str = None):
         """
         Start a game of trivia
         """
-        if topic.lower() not in Quiz.topic_list:
+        if ctx.guild.id in list(self.activeObjects.keys()):
             embed = discord.Embed(
-                title = "Trivia Error",
-                description = f"Category `{topic}` doesn't exist. Use `trivia list` to see all categories.",
-                color = 0xc1c100
-            )
+                    title = "Trivia Error",
+                    description = f"There's already a trivia session running",
+                    color = 0xf84722
+                )
             await ctx.send(embed=embed)
             return
+
+        if topic is not None:
+            if topic.lower() not in topic_list:
+                embed = discord.Embed(
+                    title = "Trivia Error",
+                    description = f"Category `{topic}` doesn't exist. Use `trivia list` to see all categories.",
+                    color = 0xf84722
+                )
+                await ctx.send(embed=embed)
+                return
 
         if amount < 1 or amount > max_questions:
             embed = discord.Embed(
                 title = "Trivia Error",
                 description = f"Invalid amount of questions. Specify a number between 1 and {max_questions}",
-                color = 0xc1c100
+                color = 0xf84722
             )
             await ctx.send(embed=embed)
             return
 
-        if difficulty.lower() not in Quiz.difficulties:
-            embed = discord.Embed(
-                title = "Trivia Error",
-                description = f"Difficulty `{difficulty}` doesn't exist. Choose `Easy`, `Medium`, `Hard`",
-                color = 0xc1c100
-            )
-            await ctx.send(embed=embed)
-            return
-
-        await ctx.send(f"{topic.lower()} {amount} {difficulty.lower()}")
+        if difficulty is not None:
+            if difficulty.lower() not in difficulties:
+                embed = discord.Embed(
+                    title = "Trivia Error",
+                    description = f"Difficulty `{difficulty}` doesn't exist. Choose `Easy`, `Medium`, `Hard`",
+                    color = 0xf84722
+                )
+                await ctx.send(embed=embed)
+                return
+        
+        quiz = Quiz(topic, amount, difficulty)
+        session = TriviaSession(ctx, quiz, self)
+        await session.start()
+        self.activeObjects[ctx.guild.id] = session
--- plugins/trivia/triviaAPI.py
+++ plugins/trivia/triviaAPI.py
@@ -1,40 +1,79 @@
 import requests
 import json
+import urllib.parse as parse
 from utils.logger import log
 
+topics = {
+    "General": 9,
+    "Books": 10,
+    "Movies": 11,
+    "Music": 12,
+    "Theatre": 13,
+    "TV": 14,
+    "Videogames": 15,
+    "Boardgames": 16,
+    "Science": 17,
+    "Computers": 18,
+    "Math": 19,
+    "Mythology": 20,
+    "Sports": 21,
+    "Geography": 22,
+    "History": 23,
+    "Politics": 24,
+    "Art": 25,
+    "Celebrities": 26,
+    "Animals": 27,
+    "Vehicles": 28,
+    "Comics": 29,
+    "Technology": 30,
+    "Anime": 31,
+    "Cartoons": 32
+}
+
+topic_list = [x.lower() for x in list(topics.keys())]
+
+difficulties = ["easy", "aedium", "hard"]
+
+class Question():
+    def __init__(self, question):
+        self.type = question["type"]
+        self.category = parse.unquote_plus(question["category"])
+        self.difficulty = question["difficulty"].title()
+        self.question = parse.unquote_plus(question["question"])
+        self.answer = parse.unquote_plus(question["correct_answer"])
+        self.wrong_answers = [parse.unquote_plus(x) for x in question["incorrect_answers"]]
+
 class Quiz():
-    topics = {
-        "General": 9,
-        "Books": 10,
-        "Movies": 11,
-        "Music": 12,
-        "Theatre": 13,
-        "TV": 14,
-        "Videogames": 15,
-        "Boardgames": 16,
-        "Science": 17,
-        "Computers": 18,
-        "Math": 19,
-        "Mythology": 20,
-        "Sports": 21,
-        "Geography": 22,
-        "History": 23,
-        "Politics": 24,
-        "Art": 25,
-        "Celebrities": 26,
-        "Animals": 27,
-        "Vehicles": 28,
-        "Comics": 29,
-        "Technology": 30,
-        "Anime": 31,
-        "Cartoons": 32
-    }
-
-    topic_list = [x.lower() for x in list(topics.keys())]
-
-    difficulties = ["easy", "aedium", "hard"]
-
     def __init__(self, topic, num_questions, difficulty):
         self.topic = topic
+        if self.topic is not None:
+            self.topic = self.topic.lower()
         self.num_questions = num_questions
-        self.difficutly = difficulty
+        self.difficulty = difficulty
+        if self.difficulty is not None:
+            self.difficulty = self.difficulty.lower()
+        self.questions = []
+        self.get_questions()
+
+    def get_questions(self):
+        self.questions.clear()
+        url = f"https://opentdb.com/api.php?amount={self.num_questions}&encode=url3986"
+        if self.topic is not None:
+            url += f"&category={topics[self.topic.title()]}"
+        if self.difficulty is not None:
+            url += f"&difficulty={self.difficulty}"
+
+        r = requests.get(url)
+        if r.status_code != 200:
+            log.error(f"Couldn't reach Trivia API: {r.status_code}")
+            return
+            
+        data = json.loads(r.text)
+        if data["response_code"] != 0:
+            log.error(f"Trivia API returned following response code: {data['response_code']}")
+            return
+
+        for question in data["results"]:
+            self.questions.append(Question(question))
+
+        
