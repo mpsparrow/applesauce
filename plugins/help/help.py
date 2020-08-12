1d75f2c151d7e2a690bce8498cbd21e6d62ed8a9
Matthew <matthew@younginwp.com>
2020-08-12 02:29:52 -0400

    more upgrades

--- plugins/help/help.py
+++ plugins/help/help.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from utils.checks import is_guild_enabled
 from utils.database.actions import connect
 from utils.config import readINI
-from utils.prefix import prefix
+from utils.prefix import prefix as getPrefix
 
 class Help(commands.Cog):
     """
@@ -21,65 +21,95 @@ class Help(commands.Cog):
         embed=discord.Embed(title="Not found.", color=0xf84722)
         await ctx.send(embed=embed)
 
-    async def command(self, ctx, command):
+    async def command_invalid(self, ctx):
+        """
+        invalid command embed
+        :param ctx:
+        """
+        embed=discord.Embed(title="Command not found.", color=0xf84722)
+        await ctx.send(embed=embed)
+
+    async def cog_invalid(self, ctx):
+        """
+        invalid cog embed
+        :param ctx:
+        """
+        embed=discord.Embed(title="Cog not found.", color=0xf84722)
+        await ctx.send(embed=embed)
+
+    async def plugin_invalid(self, ctx):
+        """
+        invalid plugin embed
+        :param ctx:
+        """
+        embed=discord.Embed(title="Plugin not found.", color=0xf84722)
+        await ctx.send(embed=embed)
+
+    async def command(self, ctx, command, prefix):
         """
         command embed
         :param ctx:
         :param command: command object
+        :param str prefix: command prefix for guild
         """
         try:
             await ctx.send("a command")
         except Exception:
             await self.error(ctx)
 
-    async def subcommand(self, ctx, subcommand):
+    async def subcommand(self, ctx, subcommand, prefix):
         """
         subcommand embed
         :param ctx:
         :param subcommand: subcommand object
+        :param str prefix: command prefix for guild
         """
         try:
             await ctx.send("a subcommand")
         except Exception:
             await self.error(ctx)
 
-    async def group(self, ctx, group):
+    async def group(self, ctx, group, prefix):
         """
         group embed
         :param ctx:
         :param group: group object
+        :param str prefix: command prefix for guild
         """
         try:
             await ctx.send("a group")
         except Exception:
             await self.error(ctx)
 
-    async def cog(self, ctx, cog):
+    async def cog(self, ctx, cog, prefix):
         """
         cog embed
         :param ctx:
         :param cog: cog object
+        :param str prefix: command prefix for guild
         """
         try:
             await ctx.send("a cog")
         except Exception:
             await self.error(ctx)
 
-    async def plugin(self, ctx, pluginData):
+    async def plugin(self, ctx, pluginData, prefix):
         """
         plugin embed
         :param ctx:
         :param plugin: plugin data from db
+        :param str prefix: command prefix for guild
         """
         try:
             await ctx.send("a plugin")
         except Exception:
             await self.error(ctx)
 
-    async def all(self, ctx):
+    async def all(self, ctx, prefix):
         """
         all help embed
         :param ctx:
+        :param str prefix: command prefix for guild
         """
         try:
             """
@@ -91,6 +121,17 @@ class Help(commands.Cog):
         except Exception:
             await self.error(ctx)
 
+    async def itemtype_help(self, ctx, prefix):
+        """
+        itemType helpf
+        :param ctx:
+        :param str prefix: command prefix for guild
+        """
+        embed=discord.Embed(title="Invalid Item Type", 
+                            description=f"**Please Use**\n`{prefix}help command <command>`\n`{prefix}help plugin <plugin>`\n`{prefix}help cog <cog>`", 
+                            color=0xf84722)
+        await ctx.send(embed=embed)
+
     @commands.command(name="help", description="Help command", usage="<plugin/command>", aliases=["h"])
     async def help(self, ctx, *, helpItem: str=None):
         """
@@ -98,10 +139,47 @@ class Help(commands.Cog):
         :param ctx:
         :param str helpItem: item that needs help
         """
+        prefix = getPrefix(ctx.guild.id)
+
         if helpItem is None:
             # display main help information
-            await self.all(ctx)
+            await self.all(ctx, prefix)
         else:
+            itemSplit = helpItem.strip().lower().split(" ")
+            itemType = [0]
+
+            if len(itemSplit) >= 2:
+                item = " ".join(itemSplit[1:])
+
+                if itemType in ["command", "commands", "com", "group", "groups", "subcommand"]:
+                    command = self.bot.get_command(item)
+
+                    if command is not None:
+                        await ctx.send("some command")
+                    else:
+                        await self.command_invalid(ctx)
+
+                elif itemType in ["p", "plugin", "plugins", "plug"]:
+                    pluginCol = connect()[readINI("config.ini")["MongoDB"]["database"]]["plugins"]
+                    pluginData = pluginCol.find_one({ "_id": item })
+
+                    if pluginData is not None:
+                        await self.plugin(ctx, pluginData, prefix)
+                    else:
+                        await self.plugin_invalid(ctx)
+                elif itemType in ["cog"]:
+                    cog = self.bot.get_cog(item)
+
+                    if cog is not None:
+                        await self.cog(ctx, cog, prefix)
+                    else:
+                        await self.cog_invalid(ctx)
+                else:
+                    # itemType didn't match
+                    await self.itemtype_help(ctx, prefix)
+
+            # old detection system
+            """
             command = self.bot.get_command(helpItem)
             cog = self.bot.get_cog(helpItem)
 
@@ -125,3 +203,4 @@ class Help(commands.Cog):
                 else:
                     # who knows what it is then
                     await self.error(ctx)
+            """
