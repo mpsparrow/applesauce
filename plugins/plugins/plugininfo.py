39506f8adddafc1a7a28015d702f621fb18d7e9d
Matthew <matthew@younginwp.com>
2020-08-12 16:02:37 -0400

    fixed authors

--- plugins/plugins/plugininfo.py
+++ plugins/plugins/plugininfo.py
@@ -2,7 +2,7 @@
 PLUGIN_NAME = "Plugin Management"
 COG_NAMES = ["Plugins"]
 VERSION = 0.1
-AUTHOR = "Matthew (mattthetechguy)"
+AUTHOR = "Lauchmelder, Matthew (mattthetechguy)"
 DESCRIPTION = "Core plugin management commands"
 LOAD_ON_START = True
 REQUIRED = True
