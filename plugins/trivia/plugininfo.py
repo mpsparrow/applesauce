1aa8c2483169bce60e23b94fadcab293bcbcdaee
Robert <robert.trololo@gmail.com>
2020-08-12 21:59:14 +0200

    Added plugin base

---
+++ plugins/trivia/plugininfo.py
@@ -0,0 +1,9 @@
+### PLUGIN INFO ###
+PLUGIN_NAME = "Trivia"
+COG_NAMES = ["Trivia"]
+VERSION = 0.1
+AUTHOR = "Lauchmelder"
+DESCRIPTION = "Play trivia against your friends!"
+LOAD_ON_START = True
+REQUIRED = True
+HIDDEN = False
