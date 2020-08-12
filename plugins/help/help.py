e3be6b05cbd7931126f06d2a602fad11ddb52a19
Matthew <matthew@younginwp.com>
2020-08-12 01:47:24 -0400

    start of custom help command

--- plugins/help/help.py
+++ plugins/help/help.py
@@ -10,43 +10,118 @@ class Help(commands.Cog):
     Custom commands
     """
     def __init__(self, bot):
-        self._original_help_command = bot.help_command
-        bot.help_command = SetupHelp()
-        bot.help_command.cog = self
+        self.bot = bot
+        self.bot.remove_command("help")
 
-class SetupHelp(commands.MinimalHelpCommand):
-    async def command_not_found(self, string):
-        embed=discord.Embed(title="Command not found.", color=0xf84722)
-        await self.context.send(embed=embed)
+    async def error(self, ctx):
+        """
+        generic error embed
+        :param ctx:
+        """
+        embed=discord.Embed(title="Not found.", color=0xf84722)
+        await ctx.send(embed=embed)
 
-    async def subcommand_not_found(self, command, string):
-        embed=discord.Embed(title="Subcommand not found.", color=0xf84722)
-        await self.context.send(embed=embed)
+    async def command(self, ctx, command):
+        """
+        command embed
+        :param ctx:
+        :param command: command object
+        """
+        try:
+            await ctx.send("a command")
+        except Exception:
+            await self.error(ctx)
 
-    # async def send_cog_help(self, cog):
+    async def subcommand(self, ctx, subcommand):
+        """
+        subcommand embed
+        :param ctx:
+        :param subcommand: subcommand object
+        """
+        try:
+            await ctx.send("a subcommand")
+        except Exception:
+            await self.error(ctx)
 
-    # async def send_group_help(self, group):
+    async def group(self, ctx, group):
+        """
+        group embed
+        :param ctx:
+        :param group: group object
+        """
+        try:
+            await ctx.send("a group")
+        except Exception:
+            await self.error(ctx)
 
-    # async def send_command_help(self, command):
+    async def cog(self, ctx, cog):
+        """
+        cog embed
+        :param ctx:
+        :param cog: cog object
+        """
+        try:
+            await ctx.send("a cog")
+        except Exception:
+            await self.error(ctx)
 
-    async def send_bot_help(self, mapping):
-        # get list of commands
-        allCmds = []
+    async def plugin(self, ctx, pluginData):
+        """
+        plugin embed
+        :param ctx:
+        :param plugin: plugin data from db
+        """
+        try:
+            await ctx.send("a plugin")
+        except Exception:
+            await self.error(ctx)
 
-        for command in set(self.context.bot.walk_commands()):
-            try:
-                await command.can_run(self.context)
-                allCmds.append(str(command))
-            except:
-                pass
+    async def all(self, ctx):
+        """
+        all help embed
+        :param ctx:
+        """
+        try:
+            """
+            embed=discord.Embed(title="Help", description="", color=0xc1c100)
+            embed.add_field(name="", value="", inline=False)
+            await ctx.send(embed=embed)
+            """
+            await ctx.send("main help")
+        except Exception:
+            await self.error(ctx)
 
-        allCmds = sorted(allCmds)
-        enableCmds = []
-        for item in allCmds:
-            if " " not in item:
-                enableCmds.append(item)
+    @commands.command(name="help", description="Help command", usage="<plugin/command>", aliases=["h"])
+    async def help(self, ctx, *, helpItem: str=None):
+        """
+        Help command
+        :param ctx:
+        :param str helpItem: item that needs help
+        """
+        if helpItem is None:
+            # display main help information
+            await self.all(ctx)
+        else:
+            command = self.bot.get_command(helpItem)
+            cog = self.bot.get_cog(helpItem)
 
-        enableString = commandList(enableCmds)
-        embed=discord.Embed(title='Help', description=f'Specify a command to get further information `{prefix(self.context.guild.id)}help <command>`', color=0xc1c100)
-        embed.add_field(name='Commands', value=f'{enableString}', inline=False)
-        await self.context.send(embed=embed)
+            if command is not None:
+                # is a command/subcommand/group
+                if command.parent is not None:
+                    # must be a subcommand
+                    await self.subcommand(command)
+                await self.command(command)
+            elif cog is not None:
+                # is a cog
+                await self.cog(ctx, cog)
+            else:
+                # either a plugin or isn't anything
+                pluginCol = connect()[readINI("config.ini")["MongoDB"]["database"]]["plugins"]
+                pluginData = pluginCol.find_one({ "_id": helpItem })
+
+                if pluginData is not None:
+                    # plugin
+                    await self.plugin(ctx, pluginData)
+                else:
+                    # who knows what it is then
+                    await self.error(ctx)
