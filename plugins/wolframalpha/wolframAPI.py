0e73c04f47b0cd7930dcbf0e43ab433caefe33de
Robert <robert.trololo@gmail.com>
2020-08-13 23:17:34 +0200

    Added DidYouMean

---
+++ plugins/wolframalpha/wolframAPI.py
@@ -0,0 +1,45 @@
+import requests
+import xml.etree.cElementTree as cET
+from urllib.parse import quote_plus
+from utils.config import readINI
+from utils.logger import log
+
+URL_TEMPLATE=f'http://api.wolframalpha.com/v2/query?appid={readINI("config.ini")["WolframAlpha"]["appID"]}&input='
+
+class WolframResult:
+    def __init__(self, query):
+        self.query = query
+        self.url = URL_TEMPLATE + quote_plus(self.query)
+        self.root = None
+
+        self.request()
+
+        self.success = False
+        self.error = "Failed to reach Wolfram|Alpha. Tell the bot provider to check their logs."
+        self.didyoumean = None
+
+        if self.root == None:
+            return
+
+        self.success = True if (self.root.get("success") == "true") else False
+
+        if not self.success:
+            dym = self.root.find("didyoumeans")
+            if dym is None:
+                self.error = "Your search returned nothing."
+                return
+            
+            self.didyoumean = dym.find("didyoumean").text
+            return
+
+        self.numpods = self.root.attrib["numpods"]
+        self.pods = []
+
+    def request(self):
+        r = requests.get(self.url)
+        if r.status_code != 200:
+            log.error(f"Failed to reach Wolfram|Alpha API: HTTP {r.status_code}")
+            return
+
+        self.root = cET.fromstring(r.text)
+        
--- plugins/wolframalpha/wolframalpha.py
+++ plugins/wolframalpha/wolframalpha.py
@@ -1,7 +1,111 @@
 import discord
 from discord.ext import commands
 from utils.checks import is_guild_enabled
+from .wolframAPI import WolframResult
+from abc import ABC, abstractmethod
+
+class WolframObject(ABC):
+    @abstractmethod
+    async def on_react_callback(self, reaction, user):
+        pass
+
+    @abstractmethod
+    async def show_embed(self):
+        pass
+
+class WolframDidYouMean(WolframObject):
+    REACTIONS = {
+        "yes": "✅",
+        "no": "❌"
+    }
+
+    def __init__(self, parent, ctx, didyoumean):
+        self.parent = parent
+        self.ctx = ctx
+        self.owner = self.ctx.author
+        self.didyoumean = didyoumean
+        
+        self.message = None
+
+    async def on_react_callback(self, reaction, user):
+        if self.message is None:
+            return
+
+        if user.id != self.owner.id:
+            return
+
+        message = reaction.message
+        if message.id != self.message.id:
+            return
+
+        if reaction.me:
+            if reaction.emoji == WolframDidYouMean.REACTIONS["yes"]:
+                await self.message.delete()
+                await self.parent.query(self.ctx, self.didyoumean)
+                return
+
+            if reaction.emoji == WolframDidYouMean.REACTIONS["no"]:
+                self.parent.activeObjects.pop(self.ctx.channel.id)
+                return
+        
+    async def show_embed(self):
+        embed = discord.Embed(
+            title = "Wolfram|Alpha Error",
+            description = f"Did you mean `{self.didyoumean}`",
+            colour = 0xf84722
+        )
+
+        self.message = await self.ctx.send(embed=embed)
+        await self.message.add_reaction(WolframDidYouMean.REACTIONS["yes"])
+        await self.message.add_reaction(WolframDidYouMean.REACTIONS["no"])
+        
+
 
 class WolframAlpha(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.activeObjects = {}
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user == self.bot.user:
+            return
+
+        channelid = reaction.message.channel.id
+        if channelid not in self.activeObjects:
+            return
+
+        await self.activeObjects[channelid].on_react_callback(reaction, user)
+
+    async def query(self, ctx, query):
+        if query is None:
+            embed = discord.Embed(
+                title = "Wolfram|Alpha Error",
+                description = "Please specify a search query: `wolf <query>`",
+                colour = 0xf84722
+            )
+            await ctx.send(embed=embed)
+            return
+
+        result = WolframResult(query)
+        if not result.success:
+            if result.didyoumean is None:
+                embed = discord.Embed(
+                    title = "Wolfram|Alpha Error",
+                    description = result.error,
+                    colour = 0xf84722
+                )
+                await ctx.send(embed=embed)
+            else:
+                self.activeObjects[ctx.channel.id] = WolframDidYouMean(self, ctx, result.didyoumean)
+                await self.activeObjects[ctx.channel.id].show_embed()
+            
+            return
+        
+        await ctx.send("Gucci Gang")
+
+    @commands.command(name="wolf", description="Searches Wolfram|Alpha", usage="<query>", aliases=["w"])
+    @is_guild_enabled()
+    async def wolf(self, ctx, *, query: str = None):
+        await self.query(ctx, query)
